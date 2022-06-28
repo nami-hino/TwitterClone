4d14563517bf4ab0fab436cbd56c40075846e220
nami-hino <n.wx27mk@gmail.com>
2022-06-28 21:30:09 +0900

    会員登録

--- home.php
+++ home.php
@@ -45,10 +45,9 @@ $view_tweets = [
  * @param string $type user | tweet
  * @return string
  */
-
-function buildimagePath(string $name = null , string $type)
+function buildImagePath(string $name = null, string $type)
 {
-    if ($type === 'user' && !isset($name)){
+    if ($type === 'user' && !isset($name)) {
         return 'Views/img/icon-default-user.svg';
     }
 
@@ -61,8 +60,6 @@ function buildimagePath(string $name = null , string $type)
     * @param string $datetime 日時
     * @return string
     */
-
-
 function convertToDayTimeAgo(string $datetime)
 {
     $unix = strtotime($datetime);
@@ -95,7 +92,6 @@ return (int)$time . $unit;
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -107,6 +103,12 @@ return (int)$time . $unit;
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo HOME_URL; ?>Views/css/style.css">
+<!-- JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous" defer></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
+<!-- いいねJS -->
+<script src="<?php echo HOME_URL; ?>Views/js/likes.js" defer></script>
 
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
@@ -123,8 +125,8 @@ return (int)$time . $unit;
                     <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-notification.svg" alt=""></a></li>
                     <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-profile.svg" alt=""></a></li>
                     <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
-                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt=""></li>
-                </ul>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-html="true" data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>"></li>
+                </ul> 
             </div>
         </div>    
      <div class="main">
@@ -160,34 +162,33 @@ return (int)$time . $unit;
                 <div class="tweet">
                     <div class="user">
                         <a href="profile.php?user_id=<?php echo htmlspecialchars($view_tweet['user_id']); ?>">
-                            <img src= "<?php echo  buildimagePath($view_tweet['user_image_name'],'user'); ?>" alt="">
+                            <img src="<?php echo buildImagePath($view_tweet['user_image_name'], 'user'); ?>" alt="">
                         </a>
                     </div>
                     <div class="content">
                         <div class="name">
                             <a href="profile.php?user_id=<?php echo htmlspecialchars($view_tweet['user_id']); ?>">
                                 <span class="nickname"><?php echo htmlspecialchars($view_tweet['user_nickname']); ?></span>
-                                <span class="user-name">@<?php echo htmlspecialchars( $view_tweet['user_name']); ?> ・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at']); ?></span>
+                                <span class="user-name">@<?php echo htmlspecialchars($view_tweet['user_name']); ?> ・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at']); ?></span>
                             </a>
                         </div>
                         <p><?php echo $view_tweet['tweet_body'] ?></p>
 
                         <?php if (isset($view_tweet['tweet_image_name'])) : ?>
-                        <img src="<?php echo  buildimagePath($view_tweet['tweet_image_name'],'tweet'); ?>" alt="" class="post-image">
+                        <img src="<?php echo buildImagePath($view_tweet['tweet_image_name'], 'tweet'); ?>" alt="" class="post-image">
                         <?php endif; ?>
 
                         <div class="icon-list">
-                            <div class="like">
+                            <div class="like js-like" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
                                 <?php
                                 if (isset($view_tweet['like_id'])) {
-                                    echo '<img src="Views/img/icon-heart-twitterblue.svg" alt="">';
+                                    echo '<img src="' . HOME_URL . 'Views/img/icon-heart-twitterblue.svg" alt="">';
                                 } else {
-                                    
-                                echo '<img src="Views/img/icon-heart.svg" alt="">';
+                                echo '<img src="' . HOME_URL . 'Views/img/icon-heart.svg" alt="">';
                                 }
                                 ?>
                             </div>
-                            <div class="like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
+                            <div class="like-count js-like-count"><?php echo htmlspecialchars($view_tweet['like_count']); ?></div>
                         </div>
                     </div>
                 </div>   
@@ -196,6 +197,11 @@ return (int)$time . $unit;
        <?php endif; ?>
      </div>
     </div>    
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            $('.js-popover').popover();
+        }, false);
+    </script>
 </body>
  
 </html>
